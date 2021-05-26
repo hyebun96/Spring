7f35c24d8afdc7e8bac64ca96dea8d93781acd73
hyebun <hyebun96@naver.com>
2021-05-26 22:52:43 +0900

    hello controller => static, mvc, api

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.engine.AttributeName;
 
 @Controller
@@ -20,5 +21,32 @@ public class HelloController {
 		model.addAttribute("name",name);
 		
 		return "hello-template";
+	}
+	
+	@GetMapping("hello-string")
+	@ResponseBody
+	public String ghelloString(@RequestParam("name") String name) {
+		return "hello " + name; // hello Spring
+	}
+	
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Hello helloApi(@RequestParam("name") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		return hello;
+	}
+	
+	static class Hello{
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
 	}
 }
